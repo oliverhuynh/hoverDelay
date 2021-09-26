5e03ea2f9e882cf38ff43d246961cd57d670a360
oliverhuynh <oliver@jufist.com>
2021-09-26 18:39:50 +0700

    Update jquery.hoverDelay.js
    Add default hover class and prettier

--- jquery.hoverDelay.js
+++ jquery.hoverDelay.js
@@ -1,39 +1,47 @@
 (function ($) {
-$.fn.hoverDelay = function(options) {
-	var defaultOptions = {
-		delayIn: 300,
-		delayOut: 300,
-		handlerIn: function(options){},
-		handlerOut: function(options){},
-		mergeHandlerIn: function(options){
-	          options.defaultHandlerIn.apply(this, options);
-	          options.handlerIn.apply(this, options);
-		},
-		mergeHandlerOut: function(options){
-		  options.defaultHandlerOut.apply(this, options);
-	          options.handlerOut.apply(this, options);
-		},
-		defaultHandlerIn: function(options){$(this).addClass('hover');},
-		defaultHandlerOut: function(options){$(this).removeClass('hover');}
-	};
-	options = $.extend(defaultOptions, options);
-	return this.each(function() {
-		var timeoutIn, timeoutOut;
-		var $element = $(this);
-			$element.hover(
-				function() {
-			    if (timeoutOut){
-		        clearTimeout(timeoutOut);
-			    }
-			    timeoutIn = setTimeout(function(){options.mergeHandlerIn.apply($element, options);}, options.delayIn);
-				},
-				function() {
-			    if (timeoutIn){
-		        clearTimeout(timeoutIn);
-			    }
-			    timeoutOut = setTimeout(function(){options.mergeHandlerOut.apply($element, options);}, options.delayOut);
-				}
-			);
+  $.fn.hoverDelay = function (options) {
+    var defaultOptions = {
+      delayIn: 300,
+      delayOut: 300,
+      handlerIn: function (options) {},
+      handlerOut: function (options) {},
+      mergeHandlerIn: function (options) {
+        options.defaultHandlerIn.call(this, options);
+        options.handlerIn.call(this, options);
+      },
+      mergeHandlerOut: function (options) {
+        options.defaultHandlerOut.call(this, options);
+        options.handlerOut.call(this, options);
+      },
+      defaultHandlerIn: function (options) {
+        $(this).addClass("hover");
+      },
+      defaultHandlerOut: function (options) {
+        $(this).removeClass("hover");
+      },
+    };
+    options = $.extend(defaultOptions, options);
+    return this.each(function () {
+      var timeoutIn, timeoutOut;
+      var $element = $(this);
+      $element.hover(
+        function () {
+          if (timeoutOut) {
+            clearTimeout(timeoutOut);
+          }
+          timeoutIn = setTimeout(function () {
+            options.mergeHandlerIn.call($element, options);
+          }, options.delayIn);
+        },
+        function () {
+          if (timeoutIn) {
+            clearTimeout(timeoutIn);
+          }
+          timeoutOut = setTimeout(function () {
+            options.mergeHandlerOut.call($element, options);
+          }, options.delayOut);
+        }
+      );
     });
-};
+  };
 })(jQuery);
