862c529a4f3237736b0dbd60379c81b7685abbc0
oliverhuynh <oliver@jufist.com>
2021-09-26 18:29:41 +0700

    Update jquery.hoverDelay.js
    Add default hover class

--- jquery.hoverDelay.js
+++ jquery.hoverDelay.js
@@ -3,8 +3,18 @@ $.fn.hoverDelay = function(options) {
 	var defaultOptions = {
 		delayIn: 300,
 		delayOut: 300,
-		handlerIn: function(){},
-		handlerOut: function(){}
+		handlerIn: function(options){},
+		handlerOut: function(options){},
+		mergeHandlerIn: function(options){
+	          options.defaultHandlerIn.apply(this, options);
+	          options.handlerIn.apply(this, options);
+		},
+		mergeHandlerOut: function(options){
+		  options.defaultHandlerOut.apply(this, options);
+	          options.handlerOut.apply(this, options);
+		},
+		defaultHandlerIn: function(options){$(this).addClass('hover');},
+		defaultHandlerOut: function(options){$(this).removeClass('hover');}
 	};
 	options = $.extend(defaultOptions, options);
 	return this.each(function() {
@@ -15,13 +25,13 @@ $.fn.hoverDelay = function(options) {
 			    if (timeoutOut){
 		        clearTimeout(timeoutOut);
 			    }
-			    timeoutIn = setTimeout(function(){options.handlerIn($element);}, options.delayIn);
+			    timeoutIn = setTimeout(function(){options.mergeHandlerIn.apply($element, options);}, options.delayIn);
 				},
 				function() {
 			    if (timeoutIn){
 		        clearTimeout(timeoutIn);
 			    }
-			    timeoutOut = setTimeout(function(){options.handlerOut($element);}, options.delayOut);
+			    timeoutOut = setTimeout(function(){options.mergeHandlerOut.apply($element, options);}, options.delayOut);
 				}
 			);
     });
